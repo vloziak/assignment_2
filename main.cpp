7bec6b0ba769ae492c7de881f08617f2f5b5c6e9
vloziak <vloziak@kse.org.ua>
2024-10-06 02:25:50 +0300

    added square

--- main.cpp
+++ main.cpp
@@ -39,10 +39,37 @@ struct Board {
                 grid[baseY][baseX] = '*';
         }
     }
+
+    void drawSquare(int x, int y, int width, int height) {
+        if (width <= 0 || height <= 0) return;
+
+        for (int j = 0; j < width; ++j) {
+            if (y >= 0 && y < BOARD_HEIGHT) {
+                if (x + j >= 0 && x + j < BOARD_WIDTH)
+                    grid[y][x + j] = '*';
+                if (y + height - 1 < BOARD_HEIGHT) {
+                    if (x + j >= 0 && x + j < BOARD_WIDTH)
+                        grid[y + height - 1][x + j] = '*';
+                }
+            }
+        }
+
+        for (int i = 1; i < height - 1; ++i) {
+            if (x >= 0 && x < BOARD_WIDTH) {
+                if (y + i >= 0 && y + i < BOARD_HEIGHT)
+                    grid[y + i][x] = '*';
+                if (x + width - 1 >= 0 && x + width - 1 < BOARD_WIDTH) {
+                    if (y + i >= 0 && y + i < BOARD_HEIGHT)
+                        grid[y + i][x + width - 1] = '*';
+                }
+            }
+        }
+    }
 };
 int main() {
     Board board;
     board.drawTriangle(10, 1, 5);
+    board.drawSquare(10,20,10,5);
     board.print();
     return 0;
 }
